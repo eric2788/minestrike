fe3f0540f01c10ee6cc94f17c45e119254d5b02c
Lam <tsukiko852@gmail.com>
2019-01-14 16:28:10 +0800

    final fix

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,9 +4,9 @@
             <v-card>
                 <v-card-title class="headline info darken-2 white--text">404 - 無法找到此頁面</v-card-title>
                 <v-divider></v-divider>
-                <v-img height="auto" src="https://sitechecker.pro/wp-content/uploads/2017/12/404.png"></v-img>
+                <v-img max-height="700px" src="https://sitechecker.pro/wp-content/uploads/2017/12/404.png"></v-img>
                 <v-card-text>
-                    <v-btn :block="isMobile" @click="$router.forward()" class="info">返回上一頁</v-btn>
+                    <v-btn :block="isMobile" @click="global.toTop()" class="info" to="/">返回主頁</v-btn>
                 </v-card-text>
             </v-card>
         </v-container>
@@ -14,8 +14,14 @@
 </template>
 
 <script>
+    import global from '../global-func'
     export default {
         name: "NotFound",
+        data() {
+            return {
+                global: global()
+            }
+        },
         computed: {
             isMobile() {
                 return this.$store.state.isMobile
